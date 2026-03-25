076774315a3ec372c76c45fd29bc70aed1ee2f9c
Labbystar <work.prod.bystar@gmail.com>
2026-03-25 17:08:56 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.config import BOT_TOKEN
 from app.db import init_db
 from app.handlers import create, menu, reminder_actions, settings, start
 from app.scheduler import start_scheduler
+from app.handlers import stats, admin
 
 
 async def main() -> None:
@@ -16,5 +17,7 @@ async def main() -> None:
     dp.include_router(menu.router)
     dp.include_router(reminder_actions.router)
     dp.include_router(settings.router)
+    dp.include_router(stats.router)
+    dp.include_router(admin.router)
     start_scheduler(bot)
     await dp.start_polling(bot)
